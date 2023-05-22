28983f352a5039bb66c82687b63465512c8b4c2e
Felipe <fmullen@illumina.com>
2023-05-22 15:31:07 -0700

    Bring in more files from new version of medusa

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -18,37 +18,34 @@ try {
 } catch (e) {
 }
 
-const plugins = [
-    `medusa-fulfillment-manual`,
-    `medusa-payment-manual`,
-    {
-        resolve: "@medusajs/admin",
-        /** @type {import('@medusajs/admin').PluginOptions} */
-        options: {
-        }
-    }
-    // {
-    //   resolve: `medusa-payment-stripe`,
-    //   options: {
-    //     api_key: process.env.STRIPE_API_KEY || '',
-    //     webhook_secret: process.env.STRIPE_WEBHOOK_SECRET || '',
-    //   },
-    // }
-];
-
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
     projectConfig: {
+        jwtSecret: process.env.JWT_SECRET,
         redis_url: process.env.REDIS_URL || 'redis://localhost:6379',
-        // For more production-like environment install PostgresQL
-        database_url: process.env.DATABASE_URL || 'postgres://localhost/medusa-store',
         database_type: 'postgres',
-        // database_database: './medusa-db.sql',
-        // database_type: 'sqlite',
+        database_url: process.env.DATABASE_URL || 'postgres://localhost/medusa-store',
         store_cors: process.env.STORE_CORS || 'http://localhost:8000',
         admin_cors: process.env.ADMIN_CORS || 'http://localhost:7000,http://localhost:7001'
     },
-    plugins,
+    plugins: [
+        `medusa-fulfillment-manual`,
+        `medusa-payment-manual`,
+        {
+            resolve: "@medusajs/admin",
+            /** @type {import('@medusajs/admin').PluginOptions} */
+            options: {
+                path: 'adm'
+            }
+        }
+        // {
+        //   resolve: `medusa-payment-stripe`,
+        //   options: {
+        //     api_key: process.env.STRIPE_API_KEY || '',
+        //     webhook_secret: process.env.STRIPE_WEBHOOK_SECRET || '',
+        //   },
+        // }
+    ],
     modules: {
         eventBus: {
             resolve: "@medusajs/event-bus-redis",
